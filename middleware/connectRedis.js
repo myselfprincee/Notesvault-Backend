342f0648c23516c59e6bd0d8005bb2154646ee19
Prince Gupta <prince.kumar01129@gmail.com>
2024-08-05 21:41:22 +0530

    fix: restored backend code

--- middleware/connectRedis.js
+++ middleware/connectRedis.js
@@ -6,25 +6,6 @@ dotenv.config({
 
 const serviceUri = process.env.REDIS_URI;
 console.log(serviceUri)
-export const redis = new Redis(serviceUri);
+export const redis = new Redis(serviceUri); 
 
-async function getAllKeys() {
-    try {
-        const key = await redis.get('note-667db2a13b3efe6e5221cd63');
-
-      console.log(JSON.parse(key));
-    //   redis.del('note-667db2a13b3efe6e5221cd63');
-    } catch (error) {
-      console.error('Error:', error);
-    }}
-
-  async function getKeyMemoryUsage(key) {
-    try {
-      const memoryUsage = await redis.memory(['usage', key]);
-      console.log(`Memory usage for ${key}: ${memoryUsage}`);
-    } catch (error) {
-      console.error('Error fetching memory usage:', error);
-    }
-  }
-  
-  getKeyMemoryUsage('note-667db2a13b3efe6e5221cd63');
+redis.flushall()
