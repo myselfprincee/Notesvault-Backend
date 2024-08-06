65ecc4ba5354756fe7a4153f15dbf646f67c9e1a
Prince Gupta <prince.kumar01129@gmail.com>
2024-08-06 15:19:54 +0530

    changes: leaving the heavy lifting on pm2

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ import authRoutes from './routes/auth.js';
 import notesRoutes from './routes/notes.js';
 import indexRoutes from './routes/index.js';
 import helmet from 'helmet';
-import cluster from 'node:cluster';
-import os from 'node:os';
-import { availableParallelism } from 'node:os';
 import process from 'node:process';
 
 connectToMongo();
@@ -22,25 +19,7 @@ app.use('/auth', authRoutes);
 app.use('/notes', notesRoutes);
 app.use('/api', indexRoutes);
 
-const numCPUs = availableParallelism();
-
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-
-} else {
   app.listen(port,'0.0.0.0', () => {
+  console.log(`NotesVault Backend listening on port ${port}`);
   console.log(`Worker ${process.pid} started`);
 });
-
-}
-
-
-console.log(os.cpus().length);
